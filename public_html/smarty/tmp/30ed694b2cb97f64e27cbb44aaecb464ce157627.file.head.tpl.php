66ee4be41745e31fa230a7de43ec033f272f9c2d
EmilyBudd <eb1338@messiah.edu>
2016-03-01 14:44:40 -0500

    New Website Files

---
+++ public_html/smarty/tmp/30ed694b2cb97f64e27cbb44aaecb464ce157627.file.head.tpl.php
@@ -0,0 +1,70 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-01 15:01:46
+         compiled from "smarty/views/head.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:866819973551c409a9ce441-41066187%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '30ed694b2cb97f64e27cbb44aaecb464ce157627' => 
+    array (
+      0 => 'smarty/views/head.tpl',
+      1 => 1427914819,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '866819973551c409a9ce441-41066187',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_551c409a9eca49_11792831',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_551c409a9eca49_11792831')) {function content_551c409a9eca49_11792831($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+<!--Brett Levengood 2015-->
+
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<`![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<?php echo '<script'; ?>
+ src="js/jquery-1.6.1.min.js"><?php echo '</script'; ?>
+>
+		<?php echo '<script'; ?>
+ src="js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+		<?php echo '<script'; ?>
+ src="js/init.js"><?php echo '</script'; ?>
+>
+        <?php echo '<script'; ?>
+ src="js/vendor/modernizr-2.7.1.min.js"><?php echo '</script'; ?>
+>
+        <style type="text/css">
+			body {
+				font-family: 'Open Sans', sans-serif;
+
+			}
+			.well {
+   				background-color: rgba(245, 245, 245, 0.8);
+			}
+			.top-buffer{
+				margin-top:10%;
+			}
+		</style>
+    </head><?php }} ?>
