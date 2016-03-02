181ddc06c942b29e9f746e1bb6881cf94304b08e
Huey-H <hh1212@messiah.edu>
2016-03-01 22:17:00 -0500

    removed a few unrelated files and renamed a few just in chase they were referenced in some places just so we have a back up copy.

---
+++ public_html/XXbentest/cache/103294a675682b49557876e7a5fb065adb720169.index.tpl.php
@@ -0,0 +1,30 @@
+<?php /*%%SmartyHeaderCode:3116656d5fa5cb04cc7-78913417%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '103294a675682b49557876e7a5fb065adb720169' => 
+    array (
+      0 => 'template\\index.tpl',
+      1 => 1428608718,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '3116656d5fa5cb04cc7-78913417',
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_56d5fa5cb68825_57683958',
+  'cache_lifetime' => 120,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_56d5fa5cb68825_57683958')) {function content_56d5fa5cb68825_57683958($_smarty_tpl) {?><!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Index</title>
+<meta charset="utf-8">
+</head>
+
+<body>
+
+<p>This is the index</p>
+
+</body>
+</html><?php }} ?>
