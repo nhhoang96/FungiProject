05553d32b16b3bf01e25f5de1da57034a919122a
nhhoang96 <hn1174@messiah.edu>
2016-04-07 14:46:50 -0400

    Updated query.php to fit the new database model

--- public_html/query.php
+++ public_html/query.php
@@ -4,17 +4,14 @@ include "../private_html/setup.php";
 //Get passed ID
 $id = $_GET["id"];
 $smarty->assign("serverID", $_SERVER['PHP_SELF']);
-//$query = "SELECT * FROM Characteristic
-//			WHERE Char_Shape_Category_ID = :parameter";
+
 $query = "SELECT Characteristic_ID, c.Name as Char_Name, Option_ID, o.Name as Opt_Name
 	FROM Characteristic c JOIN C_Option o ON Opt_Characteristic_ID = Characteristic_ID
-	WHERE Char_Shape_Category_ID = :parameter";
+	WHERE c.Category_FK = :parameter";
 $stmt = $pdo->prepare($query);
 $stmt->bindParam(':parameter', $id, PDO::PARAM_STR);
 $stmt->execute();
 
-
-//$smarty->assign("collapseVar", "collapse".$var);
 $charID = array();
 $varCount = 0;
 $cc = -1;
@@ -46,41 +43,6 @@ $charList[$cc] = $char;
 $smarty ->assign("charID", $charID);
 $smarty ->assign("charList", $charList);
 $var = 0;
-//$row2Name = array();
-
-//while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
-//	$var = $var + 1;
-// //SELECT * FROM Characteristic c JOIN
-//	$query2 = "SELECT *
-//					FROM C_Option
-//					WHERE Opt_Characteristic_ID IN
-//						(SELECT Characteristic_ID AS T2
-//						FROM Characteristic
-//						WHERE Name = '" . $row['Name'] ."' AND Char_Shape_Category_ID = :parameter2)";
-//	$stmt2 = $pdo->prepare($query2);
-//	$stmt2->bindParam(':parameter2', $id, PDO::PARAM_STR);
-//	$stmt2->execute();
-//
-
-//
-//	$rowName[$var] =$row['Name'];
-//
-//
-//	while($row2=$stmt2->fetch(PDO::FETCH_ASSOC)){
-//
-//		$row2Var = $row2Var + 1;
-//		$rowFinal = array();
-//		$rowFinal[0] = $row2['Opt_Characteristic_ID'];
-//		$rowFinal[1] = "".$opt_name;
-//		$rowFinal[2] = $row2['Option_ID'];
-//		$rowFinal[3] = $row2['Name'];
-//		$row2Name[$row2Var] = $rowFinal;
-//		$opt_name = $opt_name + 1;
-//	}
-//	//$smarty->assign("optCharID", $row2ID);
-//	$smarty->assign("row2Name", $row2Name);
-//}
-//$smarty->assign("rowName", $rowName);
 
 //<!--Process form-->
 $urlToBePassed = array();
@@ -102,42 +64,34 @@ if(isset($_POST['submit'])) {
 
 	}
 
-	$query3 = "SELECT * FROM Species 
+	$query3 = "SELECT * FROM Species
 		WHERE Species_ID IN
-			(SELECT Species_ID FROM 
+			(SELECT Species_ID FROM
 		(SELECT Species_ID, count(*) AS num
 			FROM Species_Opt LEFT JOIN Species ON Species_ID = SO_Species_ID
-			WHERE ".$build."  
+			WHERE ".$build."
 			GROUP BY Species_ID
 			HAVING num =".$c.") AS s
 		)";
 	$stmt3 = $pdo->prepare($query3);
 	$stmt3->execute();
-	//$urlToBePassed = "";
-
 
 	while($row2=$stmt3->fetch(PDO::FETCH_ASSOC)){
 		$count=$count + 1;
 		$urlToBePassed[$count] = urlencode($row2['Scientific_Name']);
-		//<li><?php echo "<a href='result.php?name=".$urlToBePassed."'>".$row2['Common_Name']."</a>" </li>
-
-		//$smarty->assign("commonName",$row2['Common_Name']);
 	}
 	$smarty->assign("urlToBePassed",$urlToBePassed);
 } else {
-	$query4 = "SELECT * FROM Species WHERE Spec_Shape_Category_ID= :parameter2";
+	$query4 = "SELECT * FROM Species WHERE Shape_FK = :parameter2";
 	$stmt4 = $pdo->prepare($query4);
 	$stmt4->bindParam(':parameter2', $id);
-//	, PDO::PARAM_STR
 	$stmt4->execute();
 	$commonName = array();
-	//$urlToBePassed = "";
+
 	while($row2=$stmt4->fetch(PDO::FETCH_ASSOC)){
 		$count=$count + 1;
 		$urlToBePassed[$count] = urlencode($row2['Scientific_Name']);
 		$commonName[$count] = $row2['Common_Name'];
-		//$smarty->assign("urlRow2","<a href='result.php?name=".$urlToBePassed."'>".$row2['Common_Name']."</a>");
-		//<li><?php echo "<a href='result.php?name=".$urlToBePassed."'>".$row2['Common_Name']."</a>"</li>
 	}
 	$smarty->assign("commonName", $commonName);
 	$smarty->assign("urlToBePassed", $urlToBePassed);
