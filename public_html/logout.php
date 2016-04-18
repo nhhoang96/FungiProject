cd3482c98b2c90e264c671a883820f3255f2cdb6
EmilyBudd <eb1338@messiah.edu>
2016-04-18 12:59:38 -0400

    Implemented Logout

--- public_html/logout.php
+++ public_html/logout.php
@@ -1,10 +1,4 @@
 <?php
-session_destroy();
-include "../private_html/setup.php";
-include_once WEB_PATH . 'CAS_includes/CAS.php';
-phpCAS::client(CAS_VERSION_2_0, 'sso.messiah.edu', 443, '/cas/',false);
-phpCAS::setNoCasServerValidation();
-phpCAS::handleLogoutRequests(false);
-phpCAS::forceAuthentication();
-$logout_url = "https://sso.messiah.edu/cas/logout";
-$_SERVER['REMOTE_USER'] = strtolower(phpCAS::getUser());
+session_start();
+session_unset();
+header('location: https://sso.messiah.edu/cas/logout');
